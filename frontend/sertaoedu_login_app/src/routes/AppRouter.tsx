7befba418545810a32679243d76bb0fe561826a9
Valdecy Lins <linsjunior99@gmail.com>
2025-05-16 12:00:50 -0300

    upp

--- frontend/sertaoedu_login_app/src/routes/AppRouter.tsx
+++ frontend/sertaoedu_login_app/src/routes/AppRouter.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { LoginPage } from '../pages/LoginPage';
 import { ProfessorDashboardPage } from '../pages/ProfessorDashboardPage';
 import { AlunoDashboardPage } from '../pages/AlunoDashboardPage';
 
 export function AppRouter() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/professor" element={<ProfessorDashboardPage />} />
-        <Route path="/aluno" element={<AlunoDashboardPage />} />
-        <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="*" element={<Navigate to="/login" replace />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/professor" element={<ProfessorDashboardPage />} />
+      <Route path="/aluno" element={<AlunoDashboardPage />} />
+      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="*" element={<Navigate to="/login" replace />} />
+    </Routes>
   );
 }
